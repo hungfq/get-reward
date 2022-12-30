7f958bed39742d2ac004a53f8e126ded264e7351
Hung Pham <t.hungpham2314@gmail.com>
2022-12-31 00:08:44 +0700

    blink

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,5 @@
+let count = 0;
+
 const spanClick = () => {
   const modal = document.getElementById("myModal");
   modal.style.display = "none";
@@ -7,7 +9,6 @@ const handleClick = () => {
   // window.alert('BẠN NHẬN ĐƯỢC 1 BÚA DÔ ĐẦU')
   const modal = document.getElementById("myModal");
   modal.style.display = "block";
-  const img = document.querySelector('body')
 }
 
 const handleMouseOver = () => {
@@ -15,9 +16,15 @@ const handleMouseOver = () => {
   img.setAttribute("style", "position:absolute;");
   const width = document.documentElement.clientWidth;
   const height = document.documentElement.clientHeight;
-  
-  img.style.left = getRandom(0, width - 300) + 'px'; // 👈🏼 Horizontally
-  img.style.top = getRandom(0, height - 300) + 'px'; // 👈🏼 Vertically
+
+  img.style.left = getRandom(0, width - 350) + 'px'; // 👈🏼 Horizontally
+  img.style.top = getRandom(0, height - 350) + 'px'; // 👈🏼 Vertically
+
+  count++;
+  if (count > 1) {
+    const blink = document.getElementById("blink");
+    blink.style.display = "none";
+  }
 }
 
 const getRandom = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
